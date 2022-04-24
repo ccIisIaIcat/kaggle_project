ab36e597541ea3578c8004114ee3f4210445338b
luke_luke <68606627+ccIisIaIcat@users.noreply.github.com>
2022-04-24 18:01:20 +0800

    Create test_1.py

---
+++ walmart_M5/lgbm_test.py/test_1.py
@@ -0,0 +1,8 @@
+#基本包的导入
+import numpy as np
+import pandas as pd
+import os, sys, gc, time, warnings, pickle, psutil, random
+#多线程包导入
+from multiprocessing import Pool        # Multiprocess Runs
+
+warnings.filterwarnings('ignore')
