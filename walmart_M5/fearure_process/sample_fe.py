249eb707dca196b107dab2c9ee355f4b3f324a35
luke_luke <68606627+ccIisIaIcat@users.noreply.github.com>
2022-04-24 16:41:19 +0800

    no description

--- walmart_M5/fearure_process/sample_fe.py
+++ walmart_M5/fearure_process/sample_fe.py
@@ -70,3 +70,78 @@ print('Load Main Data')
 train_df = pd.read_csv('E:\\kaggle_project\\kaggle_data\\walmart\\sales_train_validation.csv')
 prices_df = pd.read_csv('E:\\kaggle_project\\kaggle_data\\walmart\\sell_prices.csv')
 calendar_df = pd.read_csv('E:\\kaggle_project\\kaggle_data\\walmart\\calendar.csv')
+
+#把训练集矩阵转为id-日期-销量值三列
+#其中id索引列为'id','item_id','dept_id','cat_id','store_id','state_id'
+print('Create Grid')
+index_columns = ['id','item_id','dept_id','cat_id','store_id','state_id']
+grid_df = pd.melt(train_df, 
+                  id_vars = index_columns, 
+                  var_name = 'd', 
+                  value_name = TARGET)
+
+#相比原数据集获得了更多条数据
+print('Train rows:', len(train_df), len(grid_df))
+
+#为了后续方便预测，创建预测表格
+add_grid = pd.DataFrame()
+for i in range(1,29):
+    temp_df = train_df[index_columns]
+    temp_df = temp_df.drop_duplicates()
+    temp_df['d'] = 'd_'+ str(END_TRAIN+i)
+    temp_df[TARGET] = np.nan
+    add_grid = pd.concat([add_grid,temp_df])
+
+#把新生成的表格追加到grid后面
+grid_df = pd.concat([grid_df,add_grid])
+grid_df = grid_df.reset_index(drop=True)
+
+#把使用过的文件从内存中删除
+del temp_df, add_grid
+
+#train_df也可以删除
+del train_df
+
+#查看grid的内存使用情况
+print("{:>20}: {:>8}".format('Original grid_df',sizeof_fmt(grid_df.memory_usage(index=True).sum())))
+
+#把一些string类型转为category类型，可以节省一部分内存且不会造成有用信息的损失
+for col in index_columns:
+    grid_df[col] = grid_df[col].astype('category')
+
+#再次查看内存使用
+print("{:>20}: {:>8}".format('Reduced grid_df',sizeof_fmt(grid_df.memory_usage(index=True).sum())))
+
+#下面的操作是为了删除销量为0的列，其中这些列的wm_yr_wk列为零
+#把某一商品出现的第一次沃尔玛周作为新的一列
+release_df = prices_df.groupby(['store_id','item_id'])['wm_yr_wk'].agg(['min']).reset_index()
+release_df.columns = ['store_id','item_id','release']
+
+#把该价格合并在grid_df表中
+grid_df = merge_by_concat(grid_df, release_df, ['store_id','item_id'])
+del release_df
+
+#下面我们去删除一些零值列，这项操作需要wm_yr_wk信息
+#所以把该列合并到grid中
+grid_df = merge_by_concat(grid_df, calendar_df[['wm_yr_wk','d']], ['d'])
+
+#把零值列删除
+grid_df = grid_df[grid_df['wm_yr_wk']>=grid_df['release']]
+grid_df = grid_df.reset_index(drop=True)
+
+print(">>>>>>>>>>>>>>..")
+#查看一下grid的内存占用
+print("{:>20}: {:>8}".format('Original grid_df',sizeof_fmt(grid_df.memory_usage(index=True).sum())))
+
+#把新的release列和最小值做差，并保存为int16，进一步节约内存
+grid_df['release'] = grid_df['release'] - grid_df['release'].min()
+grid_df['release'] = grid_df['release'].astype(np.int16)
+
+#查看一下新的grid的内存占用
+print("{:>20}: {:>8}".format('Reduced grid_df',sizeof_fmt(grid_df.memory_usage(index=True).sum())))
+
+#把新的信息存起来方便后续使用
+print('Save Part 1')
+grid_df.to_pickle('kaggle_data\\walmart\\tool_data\\grid_part_1.pkl')
+
+print('Size:', grid_df.shape)
