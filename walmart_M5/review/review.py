ef99de6ff19b69a4afa188bb6ced311549c606fa
luke_luke <68606627+ccIisIaIcat@users.noreply.github.com>
2022-04-25 17:01:58 +0800

    Update review.py

--- walmart_M5/review/review.py
+++ walmart_M5/review/review.py
@@ -3,5 +3,9 @@ import pandas as pd
 
 data_1 = pd.read_pickle("E:\\kaggle_project\\kaggle_data\\walmart\\tool_data\\grid_part_1.pkl")
 data_2 = pd.read_pickle("E:\\kaggle_project\\kaggle_data\\walmart\\tool_data\\grid_part_2.pkl")
-print(data_2.head())
+data_3 = pd.read_pickle("E:\\kaggle_project\\kaggle_data\\walmart\\tool_data\\grid_part_3.pkl")
+data_4 = pd.read_pickle("E:\\kaggle_project\\kaggle_data\\walmart\\tool_data\\lags_df_28.pkl")
+print(data_4.head())
+print(list(data_4))
+print(data_4[data_4["id"]=="HOBBIES_1_008_CA_1_validation"])
 
