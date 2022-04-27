66b5f21b1db89f4fb1b89658e2b44a6d27530d8b
luke_luke <68606627+ccIisIaIcat@users.noreply.github.com>
2022-04-27 14:14:38 +0800

    Update test.py

--- jpx/test.py
+++ jpx/test.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 
-data = pd.read_csv("kaggle_data\\JPX\\train_files\\stock_prices.csv")
-print(data)
+# data = pd.read_csv("kaggle_data\\JPX\\train_files\\stock_prices.csv")
+data_2 = pd.read_csv("kaggle_data\\JPX\\stock_list.csv")
+print(data_2.loc[1000])
+print(list(data_2))
+print(data_2.info())
 
