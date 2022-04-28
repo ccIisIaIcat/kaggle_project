172eaf5d5d4341343a0e954c5710c59feed3eb09
luke_luke <68606627+ccIisIaIcat@users.noreply.github.com>
2022-04-28 12:41:22 +0800

    no description

--- jpx/judge_rule/score_judge.py
+++ jpx/judge_rule/score_judge.py
@@ -1,26 +1,27 @@
 #编写一个根据提交的submission和已有的历史数据计算得分的脚本
 
 TOTAL_STOCK = 2000 #总体的股票个数
-
 FIRST_DATE = '2017-01-04' #测试集第一天的数据
 END_DATE = '2021-12-03' #测试最后一天的数据
 
 #为方便测试，每次计算需要提交预测开始日期和结束日期
 import pandas as pd
+import warnings
+
+warnings.filterwarnings('ignore')
 
 class score_judge():
     start_test_date = ''
     end_test_date = ''
     judge_data = pd.DataFrame
     submission_dataframe = pd.DataFrame
+    data_series = pd.DataFrame
     def __init__(self,start_date,end_date,submission) -> None:
         self.start_test_date = pd.to_datetime(start_date,format = '%Y-%m-%d')
         self.end_test_date = pd.to_datetime(end_date,format = '%Y-%m-%d')
         self.submission_dataframe = submission
         self.get_dataset()
-    def test_function(self):
-        print(self.start_test_date,self.end_test_date)
-        print(self.judge_data.head())
+        self.process_submission_set()
     def get_dataset(self):
         self.judge_data = pd.read_csv("kaggle_data\\JPX\\train_files\\stock_prices.csv")
         self.judge_data = self.judge_data[['Date','SecuritiesCode','Target']]
@@ -28,27 +29,18 @@ class score_judge():
         self.judge_data = self.judge_data[(self.judge_data["Date"]>=self.start_test_date) & (self.judge_data["Date"]<=self.end_test_date)]
     def process_submission_set(self):
         self.submission_dataframe["Date"] = pd.to_datetime(self.submission_dataframe["Date"],format='%Y-%m-%d')
-        self.submission_dataframe =  self.submission_dataframe[(self.submission_dataframe["Rank"]<200) |  (self.submission_dataframe["Rank"]>=1800)]
         self.submission_dataframe = pd.merge(self.submission_dataframe,self.judge_data,how='left',on=['Date','SecuritiesCode'])
+        data_frame_up = self.submission_dataframe[self.submission_dataframe["Rank"]<200]
+        data_frame_down = self.submission_dataframe[self.submission_dataframe["Rank"]>=1800]
+        data_frame_up['tag'] = data_frame_up['Target']*(2-data_frame_up['Rank']/200)
+        data_frame_down['tag'] = data_frame_down['Target']*(2-(1999-data_frame_down['Rank'])/200)
+        self.data_series = data_frame_up.groupby(['Date'])['tag'].sum()-data_frame_down.groupby(['Date'])['tag'].sum()
+    def score(self):
+        return self.data_series.mean()/self.data_series.std()
 
 
+sample_submission = pd.read_csv("kaggle_data\\JPX\\example_test_files\\tool_sample.csv")
+s = score_judge('2020-12-04','2020-12-09',sample_submission)
+print(s.score())
 
 
-# sub = pd.read_csv("kaggle_data\\JPX\\example_test_files\\sample_submission.csv")
-# # s = score_judge('2002-4-26','2022-4-27',sub)
-# # print(pd.to_datetime(s.start_test_date,format = '%Y-%m-%d')>pd.to_datetime(s.end_test_date,format = '%Y-%m-%d'))
-# # s.test_function()
-
-# def process_submission_set(submission_dataframe):
-#         submission_dataframe["Date"] = pd.to_datetime(submission_dataframe["Date"],format='%Y-%m-%d')
-#         submission_dataframe =  submission_dataframe[(submission_dataframe["Rank"]<200) | (submission_dataframe["Rank"]>=1800)]
-#         return submission_dataframe
-# judge_data = pd.read_csv("kaggle_data\\JPX\\train_files\\stock_prices.csv")
-# judge_data = judge_data[['Date','SecuritiesCode','Target']]
-# judge_data["Date"] = pd.to_datetime(judge_data["Date"],format='%Y-%m-%d')
-# aaa = process_submission_set(sub)
-# bbb=pd.merge(aaa,judge_data,how='left',on=['Date','SecuritiesCode'])
-# print(bbb)
-judge_data = pd.read_csv("kaggle_data\\JPX\\train_files\\stock_prices.csv")
-judge_data['tag'] = (judge_data['Close']-judge_data['Open'])/judge_data['Open']
-print(judge_data)
