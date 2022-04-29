bb24a1a1b1fba519e7cb5510a4f50cb4bd0bad9e
luke_luke <68606627+ccIisIaIcat@users.noreply.github.com>
2022-04-29 15:38:13 +0800

    Update abusing_leakages_for_fun.py

--- jpx/judge_rule/abusing_leakages_for_fun.py
+++ jpx/judge_rule/abusing_leakages_for_fun.py
@@ -16,12 +16,19 @@ def calc_spread_return_sharpe(df: pd.DataFrame, portfolio_size=200, toprank_weig
     sharpe_ratio = buf.mean() / buf.std()
     return sharpe_ratio, buf
 
+def add_rank(df):
+    df["Rank"] = df.groupby("Date")["Target"].rank(ascending=False, method="first") - 1 
+    df["Rank"] = df["Rank"].astype("int")
+    return df
+
 df = pd.read_csv('kaggle_data\\JPX\\supplemental_files\\stock_prices.csv', parse_dates=["Date"])
 df = df[['Date','SecuritiesCode','Target']]
-data_day_1 = df[df['Date'] == '2021-12-06']
-data_day_2 = df[df['Date'] == '2021-12-07']
-data_day_3 = df[df['Date'] == '2021-12-08']
-print(data_day_1,data_day_2,data_day_3)
+# data_day_1 = df[(df['Date'] == '2021-12-06')|(df['Date'] == '2021-12-07')].reset_index(drop=True)
+data_day_1 = add_rank(df)
+print(calc_spread_return_sharpe(data_day_1))
+
+# def get_most_rank(df,value):
+
 
 
 
