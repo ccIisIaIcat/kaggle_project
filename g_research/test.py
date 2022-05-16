d321d883d2da49bc30a5ae42e966a273e36c1664
luke_luke <68606627+ccIisIaIcat@users.noreply.github.com>
2022-05-16 14:18:57 +0800

    Update test.py

--- g_research/test.py
+++ g_research/test.py
@@ -97,13 +97,66 @@ def reduce_mem_usage(df):
 # 读取资产信息并按照资产ID排序
 df_asset_details = pd.read_csv(ASSET_DETAILS_CSV).sort_values("Asset_ID")
 
-#读取所需的数据（注意到博主只用到了‘close’列的数据，应该是使用‘close’数据构造回滚特征
+# 读取所需的数据（注意到博主只用到了‘close’列的数据，应该是使用‘close’数据构造回滚特征
 df_train = pd.read_csv(TRAIN_CSV, usecols=['timestamp','Asset_ID', 'Close', 'Target'])
 
+# 如果要使用supplemental信息
 if use_supple_for_train:    
     df_supple = pd.read_csv(SUPPLE_TRAIN_CSV, usecols=['timestamp','Asset_ID', 'Close', 'Target'])
-#     display(df_supple)
     df_train = pd.concat([df_train, df_supple])
     del df_supple
+
+# 数据压缩
 df_train = reduce_mem_usage(df_train)
-# df_train
+
+# 这波操作算是把十四个币种每分钟的收盘价和未来十五分钟的对数收益率全部列在了一个表里吧
+# 原作者应该是敲代码新手，操作确实清奇
+train_merged = pd.DataFrame()
+for col_ in df_train.columns:
+    train_merged[col_] = 0
+for id in tqdm( range(14) ):
+    train_merged = train_merged.merge(df_train.loc[df_train["Asset_ID"] == id, ['timestamp', 'Close','Target']].copy(), on="timestamp", how='outer',suffixes=['', "_"+str(id)])   
+train_merged = train_merged.drop(df_train.columns.drop("timestamp"), axis=1)
+train_merged = train_merged.sort_values('timestamp', ascending=True)
+
+# 对于表中缺失值的填充(向前填充),不是太理解为什么要这么做
+for id in range(14):
+    train_merged[f'Close_{id}'] = train_merged[f'Close_{id}'].fillna(method='ffill', limit=100)
+
+# 一个用于得到特征变量的函数
+# 首先选出时间范围内的数据
+# 获取lags中的结束价格和均值的比值的log，整个lag期内的log收益率，
+def get_features(df, train=True):   
+    if train == True:
+        totimestamp = lambda s: np.int32(time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple()))
+        valid_window = [totimestamp("12/03/2021")]
+        print(valid_window)
+#         valid_window = [totimestamp("15/08/2021")]  #検証用
+        df['train_flg'] = np.where(df['timestamp']>=valid_window[0], 0,1)
+
+        supple_start_window = [totimestamp("22/09/2021")]
+        if use_supple_for_train:
+            df['train_flg'] = np.where(df['timestamp']>=supple_start_window[0], 1 ,df['train_flg']  )
+
+   
+    for id in range(14):    
+        for lag in lags:
+            df[f'log_close/mean_{lag}_id{id}'] = np.log( np.array(df[f'Close_{id}']) /  np.roll(np.append(np.convolve( np.array(df[f'Close_{id}']), np.ones(lag)/lag, mode="valid"), np.ones(lag-1)), lag-1)  )
+            df[f'log_return_{lag}_id{id}']     = np.log( np.array(df[f'Close_{id}']) /  np.roll(np.array(df[f'Close_{id}']), lag)  )
+    for lag in lags:
+        df[f'mean_close/mean_{lag}'] =  np.mean(df.iloc[:,df.columns.str.startswith(f'log_close/mean_{lag}_id')], axis=1)
+        df[f'mean_log_returns_{lag}'] = np.mean(df.iloc[:,df.columns.str.startswith(f'log_return_{lag}_id')] ,    axis=1)
+        for id in range(14):
+            df[f'log_close/mean_{lag}-mean_close/mean_{lag}_id{id}'] = np.array( df[f'log_close/mean_{lag}_id{id}']) - np.array( df[f'mean_close/mean_{lag}']  )
+            df[f'log_return_{lag}-mean_log_returns_{lag}_id{id}']    = np.array( df[f'log_return_{lag}_id{id}'])     - np.array( df[f'mean_log_returns_{lag}'] )
+
+    if train == True:
+        for id in range(14):
+            df = df.drop([f'Close_{id}'], axis=1)
+        oldest_use_window = [totimestamp("12/01/2019")]
+        df = df[  df['timestamp'] >= oldest_use_window[0]   ]
+
+    return df
+
+feat = get_features(train_merged)
+print(feat)
