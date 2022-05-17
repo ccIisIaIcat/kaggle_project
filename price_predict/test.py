69d593e48ae80eefb7a608991872311332da5df7
luke_luke <68606627+ccIisIaIcat@users.noreply.github.com>
2022-05-17 10:16:05 +0800

    Update test.py

--- price_predict/test.py
+++ price_predict/test.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import lightgbm as lgb
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 seed0 = 8586
 
 train_data = pd.read_csv('kaggle_data/price_predict/TrainData.csv')
@@ -39,4 +40,28 @@ params = {
     }
 
 
-print(train_data.head())
+x_data = train_data[['BEDROOMS', 'BATHROOMS', 'GARAGE', 'LAND_AREA', 'FLOOR_AREA',  'CBD_DIST', 'NEAREST_STN_DIST',  'LATITUDE', 'LONGITUDE', 'NEAREST_SCH_DIST']]
+y_data = train_data[['PRICE']]
+x_train,x_test,y_train,y_test = train_test_split(x_data,y_data,test_size = 0.3,random_state = 42)
+
+train_dataset = lgb.Dataset(x_train,y_train)
+val_dataset = lgb.Dataset(x_test,y_test,reference=train_dataset)
+
+model = lgb.train(params = params,
+                          train_set = train_dataset, 
+                          valid_sets=[train_dataset, val_dataset],
+                          valid_names=['tr', 'vl'],
+                          num_boost_round = 5000,
+                          verbose_eval = 100,     
+                        #   feval = correlation,
+                         )
+
+new_test_val = pd.read_csv('kaggle_data/price_predict/TestData.csv')
+new_test_val = new_test_val[['BEDROOMS', 'BATHROOMS', 'GARAGE', 'LAND_AREA', 'FLOOR_AREA',  'CBD_DIST', 'NEAREST_STN_DIST',  'LATITUDE', 'LONGITUDE', 'NEAREST_SCH_DIST']]
+new_test_judge = pd.read_csv('kaggle_data/price_predict/result.csv')
+new_test_judge = new_test_judge[['PRICE']]
+answer = model.predict(new_test_val)
+new_test_judge['compare'] = answer
+new_test_judge['diff'] = (new_test_judge['PRICE']-new_test_judge['compare'])/new_test_judge['PRICE']
+print(new_test_judge)
+
