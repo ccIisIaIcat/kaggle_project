2beb9c660f7b04205314c07b90b09b6a8c987bbe
luke_luke <68606627+ccIisIaIcat@users.noreply.github.com>
2022-05-18 17:56:27 +0800

    Update test_2.py

--- jpx/new_train/test_2.py
+++ jpx/new_train/test_2.py
@@ -10,12 +10,14 @@ lag_list_ = [1,2,3,5]
 
 def get_lag_info(train_data,lag_list):
     for lag_length in lag_list:
-        train_data[f'lag_{lag_length}_info'] = train_data.groupby(['SecuritiesCode'])['Open'].shift(lag_length-1)
+        train_data[f'lag_{lag_length}_info'] = train_data.groupby(['SecuritiesCode'])['Close'].shift(lag_length)
         train_data[f'lag_{lag_length}_info'] = (train_data['Close']-train_data[f'lag_{lag_length}_info'])/train_data[f'lag_{lag_length}_info']
+    train_data['target_ne'] = train_data.groupby(['SecuritiesCode'])['Target'].shift(-1)
     return train_data
 
 
 train_data_price = get_lag_info(train_data_price,lag_list_)
+train_data_price = train_data_price.drop(columns=['Open','Close'])
 
 
 new_list = train_data_price[train_data_price['SecuritiesCode'] == 1301]
