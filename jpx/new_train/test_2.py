84453ab7c398f705cef079db5991ebb13e237f2b
luke_luke <68606627+ccIisIaIcat@users.noreply.github.com>
2022-05-18 18:26:39 +0800

    Update test_2.py

--- jpx/new_train/test_2.py
+++ jpx/new_train/test_2.py
@@ -1,26 +1,35 @@
 import pandas as pd
 import numpy as np
 
+LAG_LIST = [1,2,3,5] #lag信息序列
+K_FOLDS = 7 #测试个数
+
 train_data_price = pd.read_csv('kaggle_data/JPX/train_files/stock_prices.csv')
-
-
 train_data_price = train_data_price[['Date','SecuritiesCode','Open','Close','Target']]
 
-lag_list_ = [1,2,3,5]
+
+
+date_list = train_data_price.sort_values(by='Date',ascending=True)['Date'].unique()
+print(date_list)
+print(len(date_list))
 
 def get_lag_info(train_data,lag_list):
     for lag_length in lag_list:
         train_data[f'lag_{lag_length}_info'] = train_data.groupby(['SecuritiesCode'])['Close'].shift(lag_length)
         train_data[f'lag_{lag_length}_info'] = (train_data['Close']-train_data[f'lag_{lag_length}_info'])/train_data[f'lag_{lag_length}_info']
     train_data['target_ne'] = train_data.groupby(['SecuritiesCode'])['Target'].shift(-1)
+    train_data = train_data.drop(columns=['Open','Close'])
+    train_data = train_data.dropna(axis=0,how='any')
     return train_data
 
 
-train_data_price = get_lag_info(train_data_price,lag_list_)
-train_data_price = train_data_price.drop(columns=['Open','Close'])
+# train_data_price = get_lag_info(train_data_price,lag_list_)
 
+def get_date_divide(date_list,k_folds):
 
-new_list = train_data_price[train_data_price['SecuritiesCode'] == 1301]
-print(new_list)
+    return 0
+
+# new_list = train_data_price[train_data_price['SecuritiesCode'] == 1301]
+# print(new_list)
 
         
