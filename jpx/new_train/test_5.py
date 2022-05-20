34b1586850b581152e9fcfdec914c36c1523ce3d
luke_luke <68606627+ccIisIaIcat@users.noreply.github.com>
2022-05-20 16:39:39 +0800

    Update test_5.py

--- jpx/new_train/test_5.py
+++ jpx/new_train/test_5.py
@@ -1,9 +1,27 @@
 import pandas as pd
+import numpy as np
 
 new_data = pd.read_csv('kaggle_data/JPX/train_files/stock_prices.csv')
-new_data = new_data.drop(columns=['RowId','High','Low','Volume'])
+new_data = new_data[['Date','SecuritiesCode','AdjustmentFactor']]
 print(list(new_data))
-print(new_data['AdjustmentFactor'])
-print(len(new_data[new_data['AdjustmentFactor']!= 1]))
 
-print(new_data[(new_data['SecuritiesCode']== 6861) & (new_data['Date'] <= '2017-01-20')])
+def get_adjustment(list_,lag_length):
+    list_ = np.array(list_)
+    base_point = 1
+    answer_list = []
+    for i in range(lag_length):
+        base_point = base_point*list_[i]
+        answer_list.append(base_point)
+    for i in range(len(list_)-lag_length):
+        base_point = base_point*list_[i+lag_length]/list_[i]
+        answer_list.append(base_point)
+    return answer_list
+
+new_data['adj'] = new_data.groupby(['SecuritiesCode'])['AdjustmentFactor'].transform(lambda ls_:get_adjustment(ls_,100))
+
+print(new_data[(new_data['SecuritiesCode'] == 6861) & (new_data['Date']<='2017-01-20')])
+
+
+
+
+    
