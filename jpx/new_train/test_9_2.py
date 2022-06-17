48fed61ca9100de600372ccd145e52d7915bac8d
luke_luke <68606627+ccIisIaIcat@users.noreply.github.com>
2022-06-17 10:22:22 +0800

    nd

---
+++ jpx/new_train/test_9_2.py
@@ -0,0 +1,9 @@
+#用于做一些测试
+import numpy as np
+
+combine_percent = np.linspace(start=0, stop=1, num=200)
+combine_matrix = []
+for i in range(len(combine_percent)):
+    combine_matrix.append([])
+
+print(combine_matrix)
